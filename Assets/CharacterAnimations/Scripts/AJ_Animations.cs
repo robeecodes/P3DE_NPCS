added87dd4b1ac62d70925fc3535ec3aaa9ad45e
Robin <97387960+robeecodes@users.noreply.github.com>
2024-02-02 12:17:57 +0000

    Import StarterAssets

--- Assets/CharacterAnimations/Scripts/AJ_Animations.cs
+++ Assets/CharacterAnimations/Scripts/AJ_Animations.cs
@@ -1,18 +1,25 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
-public class AJ_Animations : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class AJ_Animations : MonoBehaviour {
+    [SerializeField] private Animator animator;
+    private static readonly int Die = Animator.StringToHash("Die");
+    private static readonly int IsDancing = Animator.StringToHash("IsDancing");
+    
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.Alpha1)) {
+            animator.SetBool(IsDancing, true);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2)) {
+            animator.SetTrigger(Die);
+            animator.SetBool(IsDancing, false);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3)) {
+            animator.SetBool(IsDancing, false);
+        }
     }
 }
